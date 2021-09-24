654fb84f0f98e8b1296e16b554a3f40569d1f6df
Jeroen van den Enden <info@endroid.nl>
2021-09-25 01:23:54 +0200

    Pass Github OAuth via environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# GITHUB OAUTH
-
-if [ "$GITHUB_OAUTH_TOKEN" != "" ]; then
-    mkdir -p /var/www/.composer /root/.composer
-    chmod -R 777 /var/www/.composer /root/.composer
-    echo "{ \"github-oauth\": { \"github.com\": \"$GITHUB_OAUTH_TOKEN\" }}" > /var/www/.composer/auth.json
-    echo "{ \"github-oauth\": { \"github.com\": \"$GITHUB_OAUTH_TOKEN\" }}" > /root/.composer/auth.json
-fi
-
 # ERROR REPORTING
 
 if [ "$ENVIRONMENT" != "DEV" ]; then
